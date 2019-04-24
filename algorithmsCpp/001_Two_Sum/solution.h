f8a7ef14c18a3575de86c328eab585e2d558a1d6
huchong <1178869226@qq.com>
2019-04-24 18:17:59 +0800

    [ADD] ADD README.md
    Signed-off-by: huchong <1178869226@qq.com>

--- algorithmsCpp/001_Two_Sum/solution.h
+++ algorithmsCpp/001_Two_Sum/solution.h
@@ -6,12 +6,12 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int> &nums, int target) {
-        unordered_map<int, int> curr;
-        for (int i = 0; i < nums.size(); i++) {
-            if (curr.find(target - nums[i]) != curr.end()) {
-                return {curr[target - nums[i]], i};
+        unordered_map<int, int> nums_pairs;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums_pairs.find(target - nums[i]) != nums_pairs.end()) {
+                return {nums_pairs[target - nums[i]], i};
             }
-            curr[nums[i]] = i;
+            nums_pairs[nums[i]] = i;
         }
         return {-1, -1};
     }

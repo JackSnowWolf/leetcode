ca752cefc426c8e68c5f3c11b64432787e3f1571
huchong <1178869226@qq.com>
2019-10-11 17:42:16 -0400

    [UPDATE]

---
+++ algorithmsPython/010_Regular_Expression_Matching/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        pass
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    res = solution.isMatch("aa", "a*")
+    print(res)

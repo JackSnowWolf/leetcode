a85ec12fc19a881c4fb6a01d129685d97a6da433
huchong <1178869226@qq.com>
2019-10-22 22:07:49 -0400

    [UPDARTE] add convert BST to double linked list

---
+++ algorithmsPython/426_Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List/solution.py
@@ -0,0 +1,39 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, left, right):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+
+
+class Node:
+    def __init__(self, val, left, right):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+        def toList(curr: 'Node'):
+            nonlocal first, last
+            if curr is None:
+                return
+            if curr.left is not None:
+                toList(curr.left)
+            if last:
+                last.right = curr
+                curr.left = last
+            else:
+                first = curr
+            last = curr
+            if curr.right is not None:
+                toList(curr.right)
+
+        first, last = None, None
+        toList(root)
+        first.left = last
+        last.right = first
+        return first

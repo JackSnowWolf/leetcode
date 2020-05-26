7e66c9c6adb061975abe5105174b0ed463e7642e
huchong <1178869226@qq.com>
2020-05-26 13:31:58 -0400

    [REVISE]

--- algorithmsPython/426_Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List/solution.py
+++ algorithmsPython/426_Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List/solution.py
@@ -7,23 +7,4 @@ class Node:
 
 class Solution:
     def treeToDoublyList(self, root: 'Node') -> 'Node':
-        def toList(curr: 'Node'):
-            nonlocal first, last
-            if curr is None:
-                return
-            if curr.left is not None:
-                toList(curr.left)
-            if last:
-                last.right = curr
-                curr.left = last
-            else:
-                first = curr
-            last = curr
-            if curr.right is not None:
-                toList(curr.right)
-
-        first, last = None, None
-        toList(root)
-        first.left = last
-        last.right = first
-        return first
+        pass

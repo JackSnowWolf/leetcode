0e1e1d9d607fc530b09dd878af24cbd021df2ac4
huchong <1178869226@qq.com>
2020-06-03 22:17:42 -0400

    [REVISE]

--- algorithmsPython/010_Regular_Expression_Matching/solution.py
+++ algorithmsPython/010_Regular_Expression_Matching/solution.py
@@ -1,32 +1,43 @@
 class Solution:
+    def __init__(self):
+        self.dp = {}
+
     def isMatch(self, s: str, p: str) -> bool:
-        def compare(str1, str2) -> bool:
-            if len(str1) == 0 and len(str2) == 0:
-                return True
-            elif len(str2) == 0:
-                return False
-            elif len(str1) == 1 and len(str2) == 1:
-                if str1 == str2:
-                    return True
-                elif str1 == "." or str2 == ".":
-                    return True
-                else:
-                    return False
-            elif "*" in str2:
-                str2_split = str2.split("*", 1)
-                str2_fst = str2_split[0]
-                str2_end = str2_split[1]
-                for i in range(0, len(str1) - len(str2_fst) + 2):
-                    str2_new = str2_fst[0:-1] + str2_fst[-1] * i
-                    if compare(str1[0:len(str2_new)], str2_new):
-                        if compare(str1[len(str2_new):], str2_end):
-                            return True
-                return False
+        if (s, p) in self.dp:
+            return self.dp[s, p]
+
+        if s == p:
+            self.dp[s, p] = True
+        elif len(s) > 0 and len(p) == 0:
+            self.dp[s, p] = False
+        elif len(s) == 0 and len(p) > 0:
+            if len(p) < 2:
+                self.dp[s, p] = False
             else:
-                if len(str1) != len(str2):
-                    return False
-                return compare(str1[0], str2[0]) and compare(str1[1:], str2[1:])
-        return compare(s, p)
+                if p[1] == '*':
+                    self.dp[s, p] = self.isMatch(s, p[2:])
+                else:
+                    self.dp[s, p] = False
+        else:
+            if s[0] == p[0] or p[0] == '.':
+                if len(p) == 1:
+                    self.dp[s, p] = self.isMatch(s[1:], p[1:])
+                else:  # len(p) > 1
+                    if p[1] == '*':
+                        self.dp[s, p] = self.isMatch(s[1:], p) or self.isMatch(
+                            s, p[2:])
+                    else:
+                        self.dp[s, p] = self.isMatch(s[1:], p[1:])
+            else:
+                if len(p) == 1:
+                    self.dp[s, p] = False
+                else:
+                    if p[1] == '*':
+                        self.dp[s, p] = self.isMatch(s, p[2:])
+                    else:
+                        self.dp[s, p] = False
+
+        return self.dp[s, p]
 
 if __name__ == '__main__':
     solution = Solution()

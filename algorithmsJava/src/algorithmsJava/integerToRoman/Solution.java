a1afa870246eb4f3d7b024917a1c765f7a09c03d
huchong <1178869226@qq.com>
2020-06-05 18:23:56 -0400

    [REVISE]

--- algorithmsJava/src/algorithmsJava/integerToRoman/Solution.java
+++ algorithmsJava/src/algorithmsJava/integerToRoman/Solution.java
@@ -1,7 +1,40 @@
 package algorithmsJava.integerToRoman;
 
-class Solution {
+import java.lang.*;
+
+public class Solution {
+
+    private static int[] bins = {1000, 100, 10, 1};
+    private static char[] chars = {'M', 'C', 'X', 'I'};
+    private static char[] pairChars = {'D', 'L', 'V'};
+
+
     public String intToRoman(int num) {
-        return "";
+        assert num < 4999;
+        int rest = num;
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < bins.length; ++i) {
+            int value = rest / bins[i];
+            rest = rest % bins[i];
+            if (value == 4) {
+                out.append(chars[i]);
+                out.append(pairChars[i - 1]);
+            } else if (value == 9) {
+                out.append(chars[i]);
+                out.append(chars[i - 1]);
+            } else if (value >= 5) {
+                out.append(pairChars[i - 1]);
+                for (int j = 0; j < (value - 5); j++) {
+                    out.append(chars[i]);
+                }
+            } else if (value != 0) {
+                for (int j = 0; j < value; j++) {
+                    out.append(chars[i]);
+                }
+            }
+        }
+
+        return out.toString();
+
     }
 }
---
+++ algorithmsJava/src/algorithmsJava/integerToRoman/Test.java
@@ -0,0 +1,12 @@
+package algorithmsJava.integerToRoman;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String result = solution.intToRoman(58);
+        System.out.println(result);
+
+    }
+
+}

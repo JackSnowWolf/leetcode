84f7f8841fc0c57e4621be9470b155c12ca80a39
huchong <1178869226@qq.com>
2020-09-17 21:26:37 -0400

    [ADD] flatten binary tree to linked list

---
+++ algorithmsJava/src/algorithmsJava/nonNegativeIntegersWithoutConsecutiveOnes/Test.java
@@ -0,0 +1,9 @@
+package algorithmsJava.nonNegativeIntegersWithoutConsecutiveOnes;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.findIntegers(5);
+        System.out.println(result);
+    }
+}

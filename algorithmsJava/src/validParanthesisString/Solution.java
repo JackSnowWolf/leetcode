de8ccd981e7ebe08ee0e3831a29b1dfa587023e3
huchong <1178869226@qq.com>
2020-09-29 23:32:05 -0400

    [ADD] valid Paranthesis String

---
+++ algorithmsJava/src/validParanthesisString/Solution.java
@@ -0,0 +1,26 @@
+package validParanthesisString;
+
+class Solution {
+  public boolean checkValidString(String s) {
+    int minStack = 0;
+    int maxStack = 0;
+    for (char c : s.toCharArray()){
+      if (c == '('){
+        minStack++;
+        maxStack++;
+      }
+      else if (c == ')'){
+        minStack--;
+        maxStack--;
+        if (maxStack < 0) return false;
+        if (minStack < 0) minStack = 0;
+      }
+      else if (c == '*'){
+        if (minStack > 0) minStack--;
+        maxStack++;
+      }
+    }
+
+    return minStack == 0;
+  }
+}
